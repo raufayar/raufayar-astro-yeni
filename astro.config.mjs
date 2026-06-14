71db4b0af0b4cdea3ea8baba0c30f9fdd45777ce
raufayar <raufayarnet@gmail.com>
2026-06-15 00:24:46 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://raufayar.net',
+  site: 'https://www.raufayar.net',
   integrations: [sitemap()],
 });
