f504b8fb302cd9707ca51e14f6fbad4eb3e280b4
raufayar <raufayarnet@gmail.com>
2026-06-24 10:58:52 +0300

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.raufayar.net',
-  integrations: [sitemap()],
+  site: 'https://raufayar.net',
+  base: '/', 
 });
